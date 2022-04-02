9563083f5ae8ba01c904b25e9d15fa26b9207868
SheldonZS <sheldonzs@gmail.com>
2022-04-02 19:13:49 -0400

    Upgraded RTS movement
    Characters can move by right clicking, multiple actions can be enqueued by holding Control

--- Assets/Scripts/RTSController.cs
+++ Assets/Scripts/RTSController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RTSController : MonoBehaviour
 {
     public static RTSController instance { get; private set; }
+    public Camera camera { get; private set; }
     //[HideInInspector] 
     public GameObject selected = null;
     public List<GameObject> heroes;
@@ -12,15 +13,22 @@ public class RTSController : MonoBehaviour
     public GameObject[] monsters;
 
     public SpawnPoint[] spawnPoints;
+
+    private Transform gridAnchor;
+
+    public Commands command { get; private set; }
     // Start is called before the first frame update
 
-    void Start()
+    void Awake()
     {
         if (instance == null)
             instance = this;
         else Destroy(gameObject);
 
         selected = null;
+        camera = Camera.main;
+
+        gridAnchor = GameObject.Find("Grid Anchor").transform;
     }
 
     // Update is called once per frame
@@ -28,11 +36,14 @@ public class RTSController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit[] hits = Physics.RaycastAll(Input.mousePosition, Vector3.forward);
+            Vector3 clickWorldPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("Clicked at world position: " + clickWorldPosition);
 
-            foreach (RaycastHit hit in hits)
+            RaycastHit2D[] hits = Physics2D.RaycastAll(clickWorldPosition, Vector2.zero);
+
+            foreach (RaycastHit2D hit in hits)
             {
-                if (hit.collider.gameObject.tag == "Hero")
+                if (hit.collider.gameObject.tag == "Player")
                     selected = hit.collider.gameObject;
             }
         }
@@ -45,8 +56,15 @@ public class RTSController : MonoBehaviour
             int index = heroes.IndexOf(selected);
 
             selected = heroes[(index + 1) % heroes.Count];
+            command = Commands.idle;
         }
     }
+
+    public Vector2 MouseToGrid()
+    {
+        return gridAnchor.InverseTransformPoint(camera.ScreenToWorldPoint(Input.mousePosition));
+    }
+
 }
 
 [System.Serializable]
